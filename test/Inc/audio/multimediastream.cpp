93f58bd34d480db573ebfd1cc48baeae0edfa19f
lysgwl <lysgwl@163.com>
2017-07-24 23:21:35 +0800

    2017.07.24 23:21

--- test/Inc/audio/multimediastream.cpp
+++ test/Inc/audio/multimediastream.cpp
@@ -26,16 +26,75 @@ HRESULT MAudioStream::SetMediaStream(IMediaStream* pMediaStream)
         if ( SUCCEEDED(hr) )
 		{
 			hr = _pAudioData.CoCreateInstance(CLSID_AMAudioData, NULL, CLSCTX_INPROC_SERVER);
+			if ( SUCCEEDED(hr) )
+			{
+				hr = _pAudioStream->GetFormat(&_wfmt);
+				_pAudioData->SetFormat(&_wfmt);
+			}
+			else
+			{
+				_pAudioStream = NULL;
+			}
 		}
 	}
+
+	return hr;
 }
 
 HRESULT MAudioStream::ReleaseMediaStream()
 {
+	HRESULT hr;
+	hr = CompletionStatus(COMPSTAT_ABORT);
+	if ( SUCCEEDED(hr) )
+	{
+		_pAudioSample = NULL;
+		_pAudioData = NULL;
+		_pAudioStream = NULL;
+
+		memset(&_wfmt, 0, sizeof(_wfmt));
+	}
+
+	return hr;
 }
 
 HRESULT MAudioStream::GetSampleData(LPBYTE pbData, DWORD& dwSize, DWORD dwFlags, DWORD dwTimeout)
 {
+	HRESULT hr = E_INVALIDARG;
+
+	if ( pbData != NULL && dwSize > 0 )
+	{
+		hr = E_HANDLE;
+		if ( _pAudioStream != NULL && _pAudioData != NULL )
+		{
+			hr = S_OK;
+			if ( _pAudioSample == NULL )
+			{
+				hr = _pAudioStream->CreateSample(_pAudioData, 0, &_pAudioSample);
+			}
+
+			if ( SUCCEEDED(hr) )
+			{
+				hr = _pAudioData->SetBuffer(dwSize, pbData, 0);
+				hr = _pAudioSample->Update(SSUPDATE_ASYNC, 0, NULL, NULL);
+
+				if ( hr == MS_S_PENDING )
+				{
+					hr = _pAudioSample->CompletionStatus(dwFlags, dwTimeout);
+				}
+
+				if ( hr == MS_S_ENDOFSTREAM )
+				{
+					hr = S_FALSE;
+				}
+				if ( SUCCEEDED(hr) )
+				{
+					_pAudioData->GetInfo(NULL, NULL, &dwSize);
+				}
+			}
+		}
+	}
+
+	return hr;
 }
 
 HRESULT MAudioStream::GetSampleTimes(STREAM_TIME *pStartTime, STREAM_TIME *pEndTime, STREAM_TIME *pCurrentTime)
